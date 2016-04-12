b0af422d3087c2f2e033eb9db118fcc99b695719
Florian Gerber <floriangerber@hotmail.com>
2016-04-12 10:42:21 +0200

    Added DB creation, removed permission tab

--- classes/class.ilObjMockLearningmodule.php
+++ classes/class.ilObjMockLearningmodule.php
@@ -14,4 +14,91 @@ class ilObjMockLearningmodule extends ilObjectPlugin
     {
         $this->setType("xmlm");
     }
+
+    function doCreate()
+    {
+        global $ilDB;
+
+        $ilDB->manipulate("INSERT INTO rep_robj_xmlm_data ".
+            "(id, is_online) VALUES (".
+            $ilDB->quote($this->getId(), "integer").",".
+            $ilDB->quote(0, "integer").
+            ")");
+    }
+
+    /**
+     * Read data from db
+     */
+    function doRead()
+    {
+        global $ilDB;
+
+        $set = $ilDB->query("SELECT * FROM rep_robj_xmlm_data ".
+            " WHERE id = ".$ilDB->quote($this->getId(), "integer")
+        );
+        while ($rec = $ilDB->fetchAssoc($set))
+        {
+            $this->setOnline($rec["is_online"]);
+        }
+    }
+
+    /**
+     * Update data
+     */
+    function doUpdate()
+    {
+        global $ilDB;
+
+        $ilDB->manipulate($up = "UPDATE rep_robj_xmlm_data SET ".
+            " is_online = ".$ilDB->quote($this->getOnline(), "integer").",".
+            " WHERE id = ".$ilDB->quote($this->getId(), "integer")
+        );
+    }
+
+    /**
+     * Delete data from db
+     */
+    function doDelete()
+    {
+        global $ilDB;
+
+        $ilDB->manipulate("DELETE FROM rep_robj_xmlm_data WHERE ".
+            " id = ".$ilDB->quote($this->getId(), "integer")
+        );
+
+    }
+
+    /**
+     * Do Cloning
+     */
+    function doClone($a_target_id,$a_copy_id,$new_obj)
+    {
+        global $ilDB;
+
+        $new_obj->setOnline($this->getOnline());
+        $new_obj->update();
+    }
+
+    /**
+     * Set online
+     *
+     * @param	boolean		online
+     */
+    function setOnline($a_val)
+    {
+        $this->online = $a_val;
+    }
+
+    /**
+     * Get online
+     *
+     * @return	boolean		online
+     */
+    function getOnline()
+    {
+        return $this->online;
+    }
+
+
+
 }
--- classes/class.ilObjMockLearningmoduleGUI.php
+++ classes/class.ilObjMockLearningmoduleGUI.php
@@ -36,12 +36,13 @@ class ilObjMockLearningmoduleGUI extends ilObjectPluginGUI
     {
         switch ($cmd)
         {
-            case "editProperties":   // list all commands that need write permission here
-            case "updateProperties":
+            /*
+            case "temporary fix":   // list all commands that need write permission here
+
                 //case "...":
                 $this->checkPermission("write");
                 $this->$cmd();
-                break;
+                break; */
 
             case "showContent":   // list all commands that need read permission here
                 //case "...":
@@ -57,7 +58,7 @@ class ilObjMockLearningmoduleGUI extends ilObjectPluginGUI
      */
     function getAfterCreationCmd()
     {
-        return "editProperties";
+        return "showContent";
     }
 
     /**
@@ -89,116 +90,9 @@ class ilObjMockLearningmoduleGUI extends ilObjectPluginGUI
         // standard info screen tab
         $this->addInfoTab();
 
-        // a "properties" tab
-        if ($ilAccess->checkAccess("write", "", $this->object->getRefId()))
-        {
-            $ilTabs->addTab("properties", $this->txt("properties"),
-                $ilCtrl->getLinkTarget($this, "editProperties"));
-        }
 
         // standard epermission tab
         $this->addPermissionTab();
-    }
-
-
-// THE FOLLOWING METHODS IMPLEMENT SOME EXAMPLE COMMANDS WITH COMMON FEATURES
-// YOU MAY REMOVE THEM COMPLETELY AND REPLACE THEM WITH YOUR OWN METHODS.
-
-//
-// Edit properties form
-//
-
-    /**
-     * Edit Properties. This commands uses the form class to display an input form.
-     */
-    function editProperties()
-    {
-        global $tpl, $ilTabs;
-
-        $ilTabs->activateTab("properties");
-        $this->initPropertiesForm();
-        $this->getPropertiesValues();
-        $tpl->setContent($this->form->getHTML());
-    }
-
-    /**
-     * Init  form.
-     *
-     * @param        int        $a_mode        Edit Mode
-     */
-    public function initPropertiesForm()
-    {
-        global $ilCtrl;
-
-        include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
-        $this->form = new ilPropertyFormGUI();
-
-        // title
-        $ti = new ilTextInputGUI($this->txt("title"), "title");
-        $ti->setRequired(true);
-        $this->form->addItem($ti);
-
-        // description
-        $ta = new ilTextAreaInputGUI($this->txt("description"), "desc");
-        $this->form->addItem($ta);
-
-        // online
-        $cb = new ilCheckboxInputGUI($this->lng->txt("online"), "online");
-        $this->form->addItem($cb);
-
-        // option 1
-        $ti = new ilTextInputGUI($this->txt("option_one"), "op1");
-        $ti->setMaxLength(10);
-        $ti->setSize(10);
-        $this->form->addItem($ti);
-
-        // option 2
-        $ti = new ilTextInputGUI($this->txt("option_two"), "op2");
-        $ti->setMaxLength(10);
-        $ti->setSize(10);
-        $this->form->addItem($ti);
-
-        $this->form->addCommandButton("updateProperties", $this->txt("save"));
-
-        $this->form->setTitle($this->txt("edit_properties"));
-        $this->form->setFormAction($ilCtrl->getFormAction($this));
-    }
-
-    /**
-     * Get values for edit properties form
-     */
-    function getPropertiesValues()
-    {
-        $values["title"] = $this->object->getTitle();
-        $values["desc"] = $this->object->getDescription();
-        $values["online"] = $this->object->getOnline();
-        $values["op1"] = $this->object->getOptionOne();
-        $values["op2"] = $this->object->getOptionTwo();
-        $this->form->setValuesByArray($values);
-    }
-
-    /**
-     * Update properties
-     */
-    public function updateProperties()
-    {
-        global $tpl, $lng, $ilCtrl;
-
-        $this->initPropertiesForm();
-        if ($this->form->checkInput())
-        {
-            $this->object->setTitle($this->form->getInput("title"));
-            $this->object->setDescription($this->form->getInput("desc"));
-            $this->object->setOptionOne($this->form->getInput("op1"));
-            $this->object->setOptionTwo($this->form->getInput("op2"));
-            $this->object->setOnline($this->form->getInput("online"));
-            $this->object->update();
-            ilUtil::sendSuccess($lng->txt("msg_obj_modified"), true);
-            $ilCtrl->redirect($this, "editProperties");
-        }
-
-        $this->form->setValuesByPost();
-        $tpl->setContent($this->form->getHtml());
     }
 
 //
